1a525464f91ac3e251ca60a0e86eaa8e77a2487f
mhl <walry666@163.com>
2018-08-07 11:32:30 +0800

    change

--- Beauty/urls.py
+++ Beauty/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+import xadmin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', xadmin.site.urls),
     url(r'^account/', include('account.urls')),
     url(r'^home/', include('home.urls')),
 ]
--- home/models.py
+++ home/models.py
@@ -4,6 +4,91 @@ from datetime import date as dt
 
 # Create your models here.
 
+"""
+goods的model设计
+
+class GoodsCategory(models.Model): # 商品类别
+    CATEGORY_TYPE = {
+        (1, "一级类目"),
+        (2, "二级类目"),
+        (3, "三级类目"),
+    }
+    name = models.CharField(verbose_name="类别名", max_length=55)
+    code = models.CharField(verbose_name="类别code", max_length=55)
+    desc = models.TextField(verbose_name="类别描述", default="")
+    category_type = models.IntegerField(choices=CATEGORY_TYPE, verbose_name="类目级别")
+    parent_category = models.ForeignKey("self", null=True, blank=True, verbose_name="父类别", related_name="sub_cat")
+    is_tab = models.BooleanField(verbose_name="是否展示在导航栏", default=False)
+
+class GoodsCategoryBrand(models.Model): # 品牌名
+    name = models.CharField(verbose_name="品牌名", max_length=30, default="")
+    desc = models.TextField(verbose_name="品牌描述", max_length=200, default="")
+    image = models.ImageField(max_length=200, upload_to="brand/images/")
+
+class Goods(models.Model): # 商名
+    category = models.ForeignKey(GoodsCategory, verbose_name="商品类目")
+    goods_sn = models.CharField(verbose_name="商品唯一货号", max_length=30, primary_key=True)
+    name = models.CharField(verbose_name="商品名", max_length=30, default="")
+    click_num = models.IntegerField(verbose_name="点击数", default=0)
+    sold_num = models.IntegerField(verbose_name="商品销量", default=0)
+    fav_num = models.IntegerField(verbose_name="收藏数", default=0)
+    goods_num = models.IntegerField(verbose_name="库存数", default=0)
+    market_price = models.DecimalField(verbose_name="市场价格", max_digits=12, decimal_places=2, default=0)
+    shop_price = models.DecimalField(verbose_name="点本店价格", max_digits=12, decimal_places=2, default=0)
+    goods_brief = models.TextField(verbose_name="品牌描述", max_length=200, default="")
+    goods_desc = models.TextField(verbose_name="品牌描述", max_length=200, default="")
+    image = models.ImageField(max_length=200, upload_to="brand/images/")
+    is_new = models.BooleanField(verbose_name="是否新品", default=False)
+    is_hot = models.BooleanField(verbose_name="是否热销", default=False)
+
+"""
+
+"""
+trade的model设计
+
+class ShoppingCart(models.Model): #购物车
+    user = models.ForeignKey(User)
+    goods = models.ForeignKey(Goods)
+    nums = models.IntegerField(verbose_name="商品数量", default=0)
+
+class OrderInfo(models.Model): #订单信息
+    user = models.ForeignKey(User, verbose_name="用户")
+    order_sn = models.CharField()
+    trade_no = models.CharField()
+    pay_status = models.CharField(verbose_name="订单状态")
+    order_amount = models.DecimalField(verbose_name="订单金额")
+    address = models.CharField(verbose_name="收货地址")
+    singer_name = models.CharField(verbose_name="签收人")
+    singer_mobile = models.CharField(verbose_name="签收人手机号")
+
+class OrderGoods(models.Model): #订单的商品详情
+    order = models.ForeignKey(OrderInfo, verbose_name="订单信息")
+    goods = models.ForeignKey(Goods, verbose_name="商品")
+    goods_num = models.IntegerField(verbose_name="商品数量")
+"""
+
+"""
+用户操作的model设计
+
+class UserFac(models.Model): #用户收藏
+    user = models.ForeignKey(User, verbose_name="用户")
+    goods = models.ForeignKey(Goods, verbose_name="商品")
+
+class UserLeavingMessage(models.Model): #用户留言
+    user = models.ForeignKey(User, verbose_name="用户")
+    msg_type = models.CharField()
+    message = models.TextField()
+    file = models.FileField(upload_to="")
+
+class UserAddress(models.Model): #收货地址
+    user = models.ForeignKey(User, verbose_name="用户")
+    district = models.CharField()
+    address = models.CharField()
+    signer_name = models.CharField()
+    signer_mobile = models.CharField()
+
+"""
+
 class RequestLogs(models.Model):
     ip = models.CharField(verbose_name="服务器ip", max_length=50, null=False)
     port = models.PositiveIntegerField(verbose_name='服务器端口', null=False)
